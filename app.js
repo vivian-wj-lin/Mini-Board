edadc3df16c803d4dc3f14b379073059caed756d
Vivian Lin <vivian.wj.lin@gmail.com>
2023-01-27 23:27:38 +0800

    layout.ejs tryout. Routes folder added.

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require("express")
 const app = express()
+const expressLayouts = require("express-ejs-layouts")
+const middleware = require("./middleware")
+
+app.set("view engine", "ejs")
+app.set("views", "./views")
+app.use(expressLayouts)
+
+app.use(express.static("public"))
+
+//Routes
+const loginRoute = require("./routes/loginRoutes")
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
+  let payload = {
+    indexTitle: "come and share!",
+  }
+  res.status(200).render("index", payload)
+})
 
 app.listen(3000, function () {
   console.log("server is listening on port 3000")
---
+++ routes/loginRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const expressLayouts = require("express-ejs-layouts")
+const router = express.Router()
+
+app.set("view engine", "ejs")
+app.set("views", "./views")
+app.use(expressLayouts)
+
+router.get("/", (req, res, next) => {
+  res.status(200).render("login")
+})
+
+module.exports = router
