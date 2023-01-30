52c3e7a85f30bad1dfc34bf6aedf00d4d639bac5
Vivian Lin <vivian.wj.lin@gmail.com>
2023-01-30 23:45:58 +0800

    connected to AWS RDS

---
+++ user.js
@@ -0,0 +1,54 @@
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const S3 = require("aws-sdk/clients/s3")
+const AWS = require("aws-sdk")
+const mysql2 = require("mysql2")
+const pool = mysql2.createPool({
+  host: process.env.AWS_Mysql_Host,
+  user: process.env.AWS_Mysql_User,
+  password: process.env.AWS_Mysql_Password,
+  database: "twitter_clone",
+  port: 3306,
+})
+
+const bodyParser = require("body-parser")
+app.use(bodyParser.json({ limit: "200mb" }))
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to the database: " + err.stack)
+    return
+  }
+  console.log("Connected to MySQL as id " + connection.threadId)
+})
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to the database: " + err.stack)
+    return
+  }
+
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS user (
+      id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      username VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      profilePic varchar(10000)
+    );
+  `
+
+  connection.query(createTableQuery, (error, results) => {
+    if (error) {
+      console.error("Error creating table: " + error.stack)
+      return
+    }
+    console.log("Table created successfully.")
+  })
+
+  connection.release()
+})
+// module.exports = connection
